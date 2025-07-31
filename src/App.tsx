56865be38b1304f63dc570e41c234194387c1759
Sumit Mondal <sumitmondal@gofynd.com>
2025-07-31 23:34:04 +0530

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-import RemoteReactComponent from './components/RemoteReactComponent';
 
 const App = () => {
   return (
     <div className="content">
       <h1>React App</h1>
-      <RemoteReactComponent />
     </div>
   );
 };
